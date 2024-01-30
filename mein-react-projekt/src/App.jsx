e0fe7887f0af67f5631023f61aee69785bb9450c
Henning Koepf <90764634+gaborbatschi@users.noreply.github.com>
2024-01-30 17:27:08 +0100

    Custom Edge

--- mein-react-projekt/src/App.jsx
+++ mein-react-projekt/src/App.jsx
@@ -9,23 +9,26 @@ import ReactFlow, {
     Connection,
     Edge,
     EdgeTypes,
-    Node,
+    Node, MarkerType
+
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import './updatenode.css';
 import MenuContext from "./components/MenuContext";
-import NodeTo
+import CustomEdge from "./elements/Edge";
+
 import { data } from "./data/data";
+//import ButtonEdge from './ButtonEdge';
 
 import NewNodeButton from './buttonfunctions';
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+const initialNodes : Node[] = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1'} },
   { id: '2', position: { x: 30, y: 75 }, data: { label: '2' } },
   { id: '3', position: { x: 150, y: 150 }, data: { label: '3' } },
 ];
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' },
+const initialEdges: CustomEdge[] = [{ id: 'e1-2', source: '1', target: '2' , type: 'buttonedge'},
     { id: 'e2-3', source: '2', target: '3' ,label: 'a'}];
 
 
@@ -34,6 +37,8 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' },
 function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  //const onNodeContextMenu = useCallback((params) = > )
 
 
   const onConnect = useCallback(
@@ -47,15 +52,15 @@ function App() {
 
 
             <MenuContext data={data} />
-          <CustomNode>
-              
-          </CustomNode>
+       
+
         <ReactFlow
             nodes={nodes}
             edges={edges}
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
             onConnect={onConnect}
+            //onNodeContextMenu = {onNodeContextMenu}
         >
           <Controls />
           <MiniMap />
